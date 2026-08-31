1f2f6096f949cc07837cd84345f2979116b335fa
412-01-punyaphat <54126@yupparaj.ac.th>
2026-08-31 22:15:05 +0700

    Update bmiApp.py

--- bmiApp.py
+++ bmiApp.py
@@ -1,24 +1,28 @@
 import streamlit as st
-st.markdown("# :red[❤️ คำนวนค่าดัชนีมวลกาย BMI]")
+
+st.markdown("# :red[⚖ คำนวณค่าดัชนีมวลกาย BMI]")
 st.write("กรอกข้อมูลน้ำหนักและส่วนสูงของคุณ เพื่อเช็กสุขภาพเบื้องต้น")
 
-weight = st.number_input("กรอกข้อมูลของคุณ(กิโลกรัม):", min_value=1.0, value=1.0)
-height_cm = st.number_input("กรอกส่วนสูงของคุณ (เซนติเมตร):", min_value=1.0)
-if st.button("คำนวนค่า BMI"):
-  height_m = height_cm / 100
-  bmi = weigth / (height_m** 2)
-  
-  st.write("___")
-  st.header(f"ค่า BMI ของคุณคือ: **{bmi:.2f}**")
-  
-  if bmi < 18.5:
-    st.warning(" คุณมีน้ำหนักน้อยกว่าเกณฑ์ (ผอม)")
-  elif 18.5 <= bmi < 23.0:
-    st.success(" คุณมีน้ำหนักอยู่ในเกณฑ์ปกติ (สุขภาพดี)")
-  elif 23.0 <= bmi < 25.0:
-    st.success(" คุณเริ่มมีน้ำหนักเกินเกณฑ์ (ท้วม)")
-  else:
-    st.error(" คุณอยู่ในเกณฑ์อ้วน ควรระวังเรื่องสุขภาพและออกกำลังกาย")
+weight = st.number_input("กรอกน้ำหนักของคุณ (กิโลกรัม):", min_value=1.0, value=1.0)
+height_cm = st.number_input("กรอกส่วนสูงของคุณ (เซนติเมตร):", min_value=1.0, value=1.0)
+
+if st.button("คำนวณค่า BMI 🧮"):
+
+    # แปลงส่วนสูงจาก cm เป็น เมตร แล้วคำนวณ BMI
+    height_m = height_cm / 100
+    bmi = weight / (height_m ** 2)
+
+    st.write("---")
+    st.header(f"ค่า BMI ของคุณคือ: **{bmi:.2f}**")
+
+if bmi < 18.5:
+    st.warning("⚠️ คุณมีน้ำหนักน้อยกว่าเกณฑ์ (ผอม)")
+elif 18.5 <= bmi < 23.0:
+    st.success("✅ คุณมีน้ำหนักอยู่ในเกณฑ์ปกติ (สุขภาพดี)")
+elif 23.0 <= bmi < 25.0:
+    st.info("💡 คุณมีน้ำหนักเกินเกณฑ์ (ท้วม)")
+else:
+    st.error("🚨 คุณอยู่ในเกณฑ์อ้วน ควรระวังเรื่องสุขภาพและออกกำลังกาย")
 
 st.divider()
-st.write("นายปุญญพัตน์ ตันธิมา เลขที่1 ม.4/12")
+st.write("ปุญญพัตน์ ตันธิมา เลขที่ 1  ม.4/12")
